e3c0ac638fd5a8d74a59ebe5568b7de4dedb6815
Tautvydas Dulskis <tautvydas@dulskis.eu>
2022-12-12 14:12:59 +0200

    JS paredmenys

---
+++ js/mano-skriptas.js
@@ -0,0 +1,27 @@
+// Pirmoji užduotis
+let a = 1;
+console.log('Kintamojo a reikšmė: ' + a);
+a = 2;
+console.log('Kintamojo a reikšmė: ' + a);
+
+
+
+
+// Antroji užduotis
+let vardasPavarde = 'Tautvydas Dulskis';
+console.log('Mano vardas ir pavardė: ' + vardasPavarde);
+
+
+
+
+// Trečioji užduotis
+let metai = 1782;
+let menuo = 7;
+let diena = 25;
+let gmmd = 'Gimimo data: ' + diena + '-' + menuo + '-' + metai;
+console.log(gmmd);
+let liekanaMetai = metai % 10;
+let liekanaMenuo = menuo % 10;
+let liekanaDiena = diena % 10;
+let ats = liekanaMetai + liekanaMenuo + liekanaDiena;
+console.log('Skaitmenų suma: ' + ats );
