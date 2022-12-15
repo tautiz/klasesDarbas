d54185f57dbddeb9de2e3f9144faf2ae34332382
Tautvydas Dulskis <tautvydas@dulskis.eu>
2022-12-15 11:43:50 +0200

    JS matematikos ats

--- js/matematika.js
+++ js/matematika.js
@@ -1,49 +1,49 @@
-function add(/* ... */) {
-    // ...
+function add(a, b) {
+    return a + b;
 }
 
-function sub(/* ... */) {
-    // ...
+function sub(a, b) {
+    return a - b;
 }
 
-function div(/* ... */) {
-    // ...
+function div(a, b) {
+    return a / b;
 }
 
 function mult(a, b) {
-    // ... a * b
+    return a * b;
 }
 
 let arr1 = [1, 2, 3, 4, 5];
 let arr2 = [6, 7, 8, 9, 10];
 
+let ats = document.getElementById('ats');
 for (let i = 0; i < arr1.length; i++) {
-    // console.log(arr1[i] + arr2[i]);
-    //..
     for (let j = 0; j < arr2.length; j++) {
-        // console.log(arr2[i] + arr2[j]);
-        //..
+        ats.innerHTML += 'Skaiciu ' + arr1[i] + ' ir ' + arr2[j] + ' sudėtis yra ' + add(arr1[i], arr2[j]) + '<br>';
+        ats.innerHTML += 'Skaiciu ' + arr1[i] + ' ir ' + arr2[j] + ' atimtis yra ' + sub(arr1[i], arr2[j]) + '<br>';
+        ats.innerHTML += 'Skaiciu ' + arr1[i] + ' ir ' + arr2[j] + ' daugyba yra ' + mult(arr1[i], arr2[j]) + '<br>';
+        ats.innerHTML += 'Skaiciu ' + arr1[i] + ' ir ' + arr2[j] + ' dalyba yra ' + div(arr1[i], arr2[j]) + '<br>';
     }
 }
 
 
-
-let zenklas = "*";
-
-switch (zenklas) {
-    case "+":
-        add(/* ... */);
-        break;
-    case "-":
-        sub(/* ... */);
-        break;
-    case "*":
-        mult(/* ... */);
-        break;
-    case "/":
-        div(/* ... */);
-        break;
-    default:
-        console.log("Nepavyko");
-        break;
-}
+// let zenklas = "*";
+//
+// switch (zenklas) {
+//     case "+":
+//         add(/* ... */);
+//         break;
+//     case "-":
+//         sub(/* ... */);
+//         break;
+//     case "*":
+//         mult(/* ... */);
+//         break;
+//     case "/":
+//         div(/* ... */);
+//         break;
+//     default:
+//         console.log("Nepavyko");
+//         break;
+// }
